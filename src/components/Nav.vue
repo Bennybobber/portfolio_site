bbf3c127572a8a0702c0cfeba99b98e1cdd1aaa3
beb <bennybobber22@gmail.com>
2021-07-19 20:56:47 +0000

    basic nav complete

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,14 @@
 <template>
-    <nav>
+    <div class = "topNav" id="myTopNav">
           <router-link to="/">Home</router-link>
           <router-link to="/about">About</router-link>
           <router-link to="/contact">Contact Me</router-link>
           <router-link to="/photography">Photography Portfolio</router-link>
           <router-link to="/projects">Project Portfolio</router-link>
-          <font-awesome-icon icon="bars" />
-    </nav>
+          <router-link to="" class="icon">
+            <font-awesome-icon v-on:click="burger"  icon="bars" />
+          </router-link>
+    </div>
 </template>
 
 <script>
@@ -14,28 +16,77 @@ export default {
   name: 'Nav',
   props: {
     logoURL: String
+  },
+  methods: {
+    burger: function(event) {
+      var x = document.getElementById("myTopNav");
+    if (x.className === "topNav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topNav";
+  }
+    }
   }
 }
 </script>
 
 <style scoped>
-nav {
+.topNav {
   overflow: hidden;
   background-color: #710117;
 }
 
-nav a {
-  line-height: 3em;
+.topNav a {
   float: left;
   display:block;
-  padding: 5px;
   color: white;
   padding: 14px 16px;
+  text-align: center;
   text-decoration: none;
+  font-size: 17px;
 }
 
-#nav a.router-link-exact-active {
+.topNav a:hover{
+  background-color:#fadadd;
+  color:black;
+}
+
+.topNav .icon {
+  display: none;
+}
+
+
+@media screen and (max-width: 600px) {
+  .topNav a{display:none;}
+  .topNav a.router-link-exact-active {
+    display:block;
+  }
+  .topNav .icon {
+    float: right;
+    display: block;
+    color:white;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .topNav.responsive {position: relative;}
+  .topNav.responsive .icon {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+  .topNav.responsive a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+}
+
+.topNav a.router-link-exact-active {
   color: #ecd5bb;
+}
+.topNav a.router-link-exact-active:hover {
+  color: black;
 }
 
 </style>
